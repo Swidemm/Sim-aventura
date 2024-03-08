4b92a532fb37c78e606337208998a8966f35b80f
seidem <lauti.seid@gmail.com>
2024-03-07 21:33:51 -0300

    adicion de funciones, falta bug fixing

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,33 @@
-let inicio = true
 while (true) {
-        let Confirmacion = prompt("Bienvenido, deseas jugar?  \n1. Si \n2. No");
+
+    let Confirmacion = prompt("Bienvenido, deseas jugar?  \n1. Si \n2. No");
+
     if (Confirmacion == 1) {
+
         Aventura = true;
-        break
+        break;
+
     } else if (Confirmacion == 2) {
+
         alert("Que lastima...")
         Aventura = false;
 
         break;
+
     } else {
+
         alert("Por favor, seleccione una opcion correspondiente")
+
     }
 }
+
 while (Aventura == true) {
-    alert("Genial, comencemos... recuerda que debes responder con el numero de la opcion correspondiente.");
+
+    let NombrePersona = prompt( "Por favor, primero indicame tu nombre...")
+
+    let NombrePersonaje = prompt("Genial, ahora por favor, indicame el nombre por el cual te gustaria llamarte en el juego...")
+    
+    saludajugador(NombrePersona, NombrePersonaje);
 
     alert("Te encuentras caminando hacia tu casa luego de un largo dia de trabajo, ya hace tiempo que tienes en mente comprar una casa...");
 
@@ -22,29 +35,16 @@ while (Aventura == true) {
 
     alert("Te doy la bienvenida, me llamo Ezequiel, ¿Cual es tu nombre?");
 
-    let NombrePersonaje = prompt ("Me llamo ...");
-
+    alert("Respondes: Me llamo " + NombrePersonaje + ".");
 
     alert("Bienvenido/a seas " + NombrePersonaje + " " + "decime, ¿en que puedo ayudarte?");
 
     alert("Respondes: Deseaba comprar una casa, o un terreno. ¿que opciones tienen disponibles?");
 
     alert("Excelente, tenemos una amplia variedad de terrrenos y casas prefabricadas, tambien podemos hacerla a medida, pero eso requiere mas tiempo. en el catalogo a tu lado podras elegir el terreno y casa que desees y te dare un aproximado del valor.");
-    
-    let Terreno = prompt ("Por favor, selecciona cual deseas (1, 2 o 3)\n1. Terreno de 9.14m x 28m \n2. Terreno de 12m x 28m \n3. Terreno de 12m x 42.50m");
-    if (Terreno == 1) {
-        ValorTerreno = 3000;
-        TamañoTotal = "335.00m2";
-        RecomendacionCasa = "pequeña con un garage lateral o un muy pequeño patio delantero, despues dediciras cual prefieres";
-    } else if (Terreno == 2) {
-        ValorTerreno = 4000;
-        TamañoTotal = "340.00m2";
-        RecomendacionCasa = "mediana con garage lateral y un pequeño patio delantero";
-    } else if (Terreno == 3) {
-        ValorTerreno = 9000;
-        TamañoTotal = "510.00m2";
-        RecomendacionCasa = "grande con patio delantero y garage lateral";
-    }
+
+    haceventaterreno();
+
     alert("Excelente eleccion, el tamaño total es de " + TamañoTotal + " " + "yo te recomendaria una casa " + RecomendacionCasa + " " +  "para aprovechar bien el espacio...");
 
     alert("Volviendo al terreno, este mismo tiene un valor de " + ValorTerreno + " " + "en mi opinion, te conviene comprar tanto el terreno como la casa prefabricada, ya que las tenemos en descuento!");
@@ -55,7 +55,56 @@ while (Aventura == true) {
 
     if (TipoCasa == 1) {
 
-        let CasaPrefab = prompt("Genial, por favor, selecciona cual deseas (1, 2 o 3) \n1. Prefabricada de lujo \n2. Prefabricada clase media \n3. Prefabricada basica")
+        ending1();
+
+        ending1post(NombrePersonaje, TamañoTotal, TipoCasaPrefab, Total, NombrePersona);
+
+        break;
+
+    } else if (TipoCasa == 2) {
+
+        ending2();
+
+        ending2post(NombrePersonaje, TamañoTotal, NombrePersona);
+
+        break;
+
+    }
+}
+
+// funciones de hola :D
+
+function saludajugador(persona, personaje) {
+
+    alert("¡Bienvenido seas a mi aventura " + persona + " " + "espero que disfrutes esta pequeña experiencia! Veo que tu personaje se llama " + personaje + " " + "este mismo nombre sera utilizado a lo largo de nuestra aventura!")
+    alert("¿Preparado? ¡Comencemos! (recuerda que debes responder con el numero de la opcion correspondiente.)")
+
+}
+
+// funciones de venta
+
+function haceventaterreno() {
+
+    let Terreno = prompt ("Por favor, selecciona cual deseas (1, 2 o 3)\n1. Terreno de 9.14m x 28m \n2. Terreno de 12m x 28m \n3. Terreno de 12m x 42.50m");
+
+        if (Terreno == 1) {
+            ValorTerreno = 3000;
+            TamañoTotal = "335.00m2";
+            RecomendacionCasa = "pequeña con un garage lateral o un muy pequeño patio delantero, despues dediciras cual prefieres";
+        } else if (Terreno == 2) {
+            ValorTerreno = 4000;
+            TamañoTotal = "340.00m2";
+            RecomendacionCasa = "mediana con garage lateral y un pequeño patio delantero";
+        } else if (Terreno == 3) {
+            ValorTerreno = 9000;
+            TamañoTotal = "510.00m2";
+            RecomendacionCasa = "grande con patio delantero y garage lateral";
+        }
+}
+
+function haceventacasa() {
+
+    let CasaPrefab = prompt("Genial, por favor, selecciona cual deseas (1, 2 o 3) \n1. Prefabricada de lujo \n2. Prefabricada clase media \n3. Prefabricada basica")
 
         if (CasaPrefab == 1) {
             ValorCasa = 12000;
@@ -68,35 +117,58 @@ while (Aventura == true) {
             TipoCasaPrefab = "basica"
         }
 
-        let Total = ValorCasa + ValorTerreno
-        alert("Asi que una casa " + TipoCasaPrefab + " " + "eh? excelente eleccion! esa misma vale " + ValorCasa + ".")
+}
 
-        alert("Excelente, entonces seria... " + Total + ".")
+// choclo de funciones de final
 
-        alert("Respondes: Genial entonces! ¿Para cuando estaria todo esto?");
+function ending1() {
 
-        alert("Para aproximadamente 7 dias habiles desde que pagas");
+    haceventacasa();
 
-        let ModoPago = prompt("Pago con... \n1. efectivo \n2. debito \n3. credito ");
+    let Total = ValorCasa + ValorTerreno
+    
+    alert("Asi que una casa " + TipoCasaPrefab + " " + "eh? excelente eleccion! esa misma vale " + ValorCasa + ".")
 
-        let NumeroCelularTotal = prompt("Excelente eleccion, ya mismo te cobro... ¡y listo! Ya es todo tuyo, en los proximos dias habiles me voy a estar comunicando para aclarar los detalles, por favor, dejame tu numero de telefono...");
+    alert("Excelente, entonces seria... " + Total + ".")
 
-        alert("Excelente, entonces tu numero es " + NumeroCelularTotal + " " + "¿cierto? Muy bien, me comunicare contigo en los proximos 7 dias habiles, ¡Que te vaya mas que bien!");
+    alert("Respondes: Genial entonces! ¿Para cuando estaria todo esto?");
 
-        alert("Sales del local, con una sonrisa por la gran atencion y precios, luego de unas cuadras, llegas a tu departamento, ilusionado de haber logrado tener tu propia casa, te acuestas y terminas el dia.");
+    alert("Para aproximadamente 7 dias habiles desde que abonas");
 
-        break;
+    let ModoPago = prompt("Pago con... \n1. efectivo \n2. debito \n3. credito ");
 
-    } else if (TipoCasa == 2) {
+    let NumeroCelularTotal = prompt("Excelente eleccion, ya mismo te cobro... ¡y listo! Ya es todo tuyo, en los proximos dias habiles me voy a estar comunicando para aclarar los detalles, por favor, dejame tu numero de telefono...");
 
-        alert("Genial, por favor anotame tu numero de celular en esta libreta asi me comunico");
+    alert("Excelente, entonces tu numero es " + NumeroCelularTotal + " " + "¿cierto? Muy bien, me comunicare contigo en los proximos 7 dias habiles, ¡Que te vaya mas que bien!");
 
-        let NumeroCelular = prompt ("Agarras la libreta y escribes tu numero de celular...");
+    alert("Sales del local, con una sonrisa por la gran atencion y precios, luego de unas cuadras, llegas a tu departamento, ilusionado de haber logrado tener tu propia casa, te acuestas y terminas el dia.");
 
-        alert("Excelente, entonces tu numero es " + NumeroCelular + " " + "¿cierto? Muy bien, me comunicare contigo en las proximas 24 horas, ¡Que te vaya mas que bien!");
+}
+function ending2() {
+    
+    alert("Genial, por favor anotame tu numero de celular en esta libreta asi me comunico");
 
-        alert("Sales del local, con una sonrisa por la gran atencion y precios, luego de unas cuadras, llegas a tu departamento y terminas el dia.");
+    let NumeroCelular = prompt ("Agarras la libreta y escribes tu numero de celular...");
 
-        break;
-    }
+    alert("Excelente, entonces tu numero es " + NumeroCelular + " " + "¿cierto? Muy bien, me comunicare contigo en las proximas 24 horas, ¡Que te vaya mas que bien!");
+
+    alert("Sales del local, con una sonrisa por la gran atencion y precios, luego de unas cuadras, llegas a tu departamento y terminas el dia.");
+
+
+}
+
+function ending1post(personaje, tamanoterreno, tipoprefab, totalgastado, persona) {
+
+    alert("¡Llegaste al final! ahora, veamos un poco de estadisticas: " + "Tu personaje " + personaje + "compro un terreno de tamaño " + tamanoterreno + " " + "en este mismo, coloco una casa prefabricada " + tipoprefab + " " + "gastando un total de: " + totalgastado + " " + "¡Felicidades! Tienes tu casa de ensueño.")
+
+    alert("¡Muchisimas gracias por jugar mi aventura! " + persona + " " + "Se vienen expansiones a la historia, con multiples finales y mayor interactividad!")
+
+}
+
+function ending2post(personaje, tamanoterreno, persona) {
+
+    alert("¡Llegaste al final! ahora, veamos un poco de estadisticas: " + "Tu personaje " + personaje + "compro un terreno de tamaño " + tamanoterreno + " " + "sin embargo, decidio no comprar una casa prefabricada, sino que armar la suya propia.")
+    
+    alert("¡Muchisimas gracias por jugar mi aventura! " + persona + " " + "Se vienen expansiones a la historia, con multiples finales y mayor interactividad!")
+
 }

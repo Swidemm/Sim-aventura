046b726ed37f71766213a25f45e2d0184a765d32
seidem <lauti.seid@gmail.com>
2024-03-18 11:39:58 -0300

    fixeo de errores, adicion de control de errores en seleccion de tipocasa

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ while (true) {
         Aventura = false;
         break;
     } else {
-        alert("Por favor, seleccione una opcion correspondiente")
+        alert("Por favor, seleccione una opcion correspondiente");
     }
 }
 
@@ -23,7 +23,8 @@ while (Aventura === true) {
     // Codigo para que el nombre ingresado siempre tenga la primer letra mayuscula y lo demas minuscula
     NombrePersona = NombrePersona.charAt(0).toUpperCase() + NombrePersona.slice(1).toLowerCase();
 
-    console.log("Nombre ingresado: " + NombrePersona);
+    console.log("Nombre ingresado: " + NombrePersona); 
+    // existe para debuggear
 
     let NombrePersonaje;
 
@@ -34,7 +35,8 @@ while (Aventura === true) {
     // Codigo para que el nombre ingresado siempre tenga la primer letra mayuscula y lo demas minuscula
     NombrePersonaje = NombrePersonaje.charAt(0).toUpperCase() + NombrePersonaje.slice(1).toLowerCase();
     
-    console.log("Nombre del personaje ingresado: " + NombrePersonaje);
+    console.log("Nombre del personaje ingresado: " + NombrePersonaje); 
+    // existe para debuggear
     
     saludajugador(NombrePersona, NombrePersonaje);
 
@@ -54,32 +56,35 @@ while (Aventura === true) {
 
     haceventaterreno();
 
+    console.log("tamaño de terreno seleccionado: " + TamanoTotal + ", " + "de valor" + ValorTerreno); 
+    // existe para debuggear
+
     alert("Excelente eleccion, el tamaño total es de " + TamanoTotal + " " + "yo te recomendaria una casa " + RecomendacionCasa + " " +  "para aprovechar bien el espacio...");
 
     alert("Volviendo al terreno, este mismo tiene un valor de " + ValorTerreno + " " + "en mi opinion, te conviene comprar tanto el terreno como la casa prefabricada, ya que las tenemos en descuento!");
 
     alert("Ahora, vamos por la casa, dime, ¿deseas una prefabricada o quisieras que me comunique contigo para hacer una a medida?") ;
 
-    let TipoCasa = prompt("Me gustaria... \n1. Una casa prefabricada \n2. Que te comuniques conmigo para hacer una a medida");
 
-    if (TipoCasa === 1) {
+    let TipoCasa = prompt("Me gustaría... \n1. Una casa prefabricada \n2. Que te comuniques conmigo para hacer una a medida");
+
+        // Valida la entrada de usuario y repite el prompt si es necesario
+        while (TipoCasa !== "1" && TipoCasa !== "2") {
+            TipoCasa = prompt("Por favor, selecciona una opción válida: \n1. Una casa prefabricada \n2. Que te comuniques conmigo para hacer una a medida");
+        }
         
-        do {
-            TipoCasa = prompt("Me gustaría... \n1. Una casa prefabricada \n2. Que te comuniques conmigo para hacer una a medida");
-          
-            if (TipoCasa !== "1" && TipoCasa !== "2") {
-                alert("Por favor, seleccione una opción válida. (1 o 2)");
-            }
+    if (TipoCasa == 1) {
+        
+        haceventacasa();
 
-        } while (TipoCasa !== "1" && TipoCasa !== "2");
+        console.log("Tipo de casa seleccionada: " + TipoCasaPrefab + " " + "de valor " + ValorCasa); 
+        // existe para debuggear
 
-        console.log("Tipo de casa seleccionada: " + TipoCasaPrefab + " " + "de valor " + ValorCasa)
-
-        let Montototal = ValorCasa + ValorTerreno
+        let Montototal = ValorCasa + ValorTerreno;
     
-        alert("Asi que una casa " + TipoCasaPrefab + " " + "eh? excelente eleccion! esa misma vale " + ValorCasa + ".")
+        alert("Asi que una casa " + TipoCasaPrefab + " " + "eh? excelente eleccion! esa misma vale " + ValorCasa + ".");
     
-        alert("Excelente, entonces seria... " + Montototal + ".")
+        alert("Excelente, entonces seria... " + Montototal + ".");
     
         alert("Respondes: Genial entonces! ¿Para cuando estaria todo esto?");
     
@@ -109,7 +114,8 @@ while (Aventura === true) {
             NumeroCelularTotal = prompt("Excelente elección, ya mismo te cobro... ¡y listo! Ya es todo tuyo, en los próximos días hábiles me voy a estar comunicando para aclarar los detalles. Por favor, déjame tu número de teléfono:");
         } while (!NumeroCelularTotal || !/^\d+$/.test(NumeroCelularTotal) || NumeroCelularTotal.length < 4);
         
-        console.log("Numero de celular ingresado:" + " " + NumeroCelularTotal)
+        console.log("Numero de celular ingresado:" + " " + NumeroCelularTotal); 
+        // existe para debuggear
     
         alert("Excelente, entonces tu numero es " + NumeroCelularTotal + " " + "¿cierto? Muy bien, me comunicare contigo en los proximos 7 dias habiles, ¡Que te vaya mas que bien!");
     
@@ -119,11 +125,18 @@ while (Aventura === true) {
 
         break;
 
-    } else if (TipoCasa === 2) {
+    } else if (TipoCasa == 2) {
 
         alert("Genial, por favor anotame tu numero de celular en esta libreta asi me comunico");
 
-        let NumeroCelular = prompt ("Agarras la libreta y escribes tu numero de celular...");
+        let NumeroCelular;
+
+        do {
+            NumeroCelular = prompt("Agarras la libreta y escribes tu numero de celular...");
+        } while (!NumeroCelular || !/^\d+$/.test(NumeroCelular) || NumeroCelular.length < 4);
+
+        console.log("Numero de celular ingresado:" + " " + NumeroCelular); 
+        // existe para debuggear
     
         alert("Excelente, entonces tu numero es " + NumeroCelular + " " + "¿cierto? Muy bien, me comunicare contigo en las proximas 24 horas, ¡Que te vaya mas que bien!");
     
@@ -135,14 +148,13 @@ while (Aventura === true) {
 
     }
 }
-
 // funciones de hola :D
 
 function saludajugador(persona, personaje) {
 
-    alert("¡Bienvenido seas a mi aventura " + persona + " " + "espero que disfrutes esta pequeña experiencia! Veo que tu personaje se llama " + personaje + " " + "este mismo nombre sera utilizado a lo largo de nuestra aventura!")
+    alert("¡Bienvenido seas a mi aventura " + persona + " " + "espero que disfrutes esta pequeña experiencia! Veo que tu personaje se llama " + personaje + " " + "este mismo nombre sera utilizado a lo largo de nuestra aventura!");
     
-    alert("¿Preparado? ¡Comencemos! (recuerda que debes responder con el numero de la opcion correspondiente.)")
+    alert("¿Preparado? ¡Comencemos! (recuerda que debes responder con el numero de la opcion correspondiente.)");
 
 }
 
@@ -153,7 +165,8 @@ function haceventaterreno() {
 
     // control de errores para que el usuario elija la opcion correcta
     while (Terreno !== "1" && Terreno !== "2" && Terreno !== "3") {
-        Terreno = prompt("Por favor, selecciona una opción válida (1, 2 o 3)");
+        // Muestra las opciones nuevamente
+        Terreno = prompt("Por favor, selecciona una opción válida (1, 2 o 3)\n1. Terreno de 9.14m x 28m \n2. Terreno de 12m x 28m \n3. Terreno de 12m x 42.50m");
     }
 
     if (Terreno === "1") {
@@ -171,12 +184,15 @@ function haceventaterreno() {
     }
 }
 
+
 function haceventacasa() {
-    let CasaPrefab = prompt("Genial, por favor, selecciona cual deseas (1, 2 o 3) \n1. Prefabricada de lujo \n2. Prefabricada clase media \n3. Prefabricada básica");
+
+    let CasaPrefab = prompt ("Por favor, selecciona cual deseas (1, 2 o 3)\n1. Prefabricada de lujo \n2. Prefabricada clase media \n3. Prefabricada básica");
 
     // control de errores para que el usuario elija la opcion correcta
     while (CasaPrefab !== "1" && CasaPrefab !== "2" && CasaPrefab !== "3") {
-        CasaPrefab = prompt("Por favor, selecciona una opción válida (1, 2 o 3)");
+        // Muestra las opciones nuevamente
+        CasaPrefab = prompt("Por favor, selecciona una opción válida (1, 2 o 3)\n1. Prefabricada de lujo \n2. Prefabricada clase media \n3. Prefabricada básica");
     }
 
     if (CasaPrefab === "1") {
@@ -195,18 +211,16 @@ function haceventacasa() {
 
 function ending1post(personaje, tamanoterreno, tipoprefab, totalgastado, persona) {
 
-    alert("¡Llegaste al final! ahora, veamos un poco de estadisticas: " + "Tu personaje " + personaje + " " + "compro un terreno de tamaño " + tamanoterreno + " " + "en este mismo, coloco una casa prefabricada " + tipoprefab + " " + "gastando un total de: " + totalgastado + " " + "¡Felicidades! Tienes tu casa de ensueño.")
+    alert("¡Llegaste al final! ahora, veamos un poco de estadisticas: " + "Tu personaje " + personaje + " " + "compro un terreno de tamaño " + tamanoterreno + " " + "en este mismo, coloco una casa prefabricada " + tipoprefab + " " + "gastando un total de: " + totalgastado + " " + "¡Felicidades! Tienes tu casa de ensueño.");
 
-    alert("¡Muchisimas gracias por jugar mi aventura! " + persona + "," + " " + "se vienen expansiones a la historia, con multiples finales y mayor interactividad!")
-
+    alert("¡Muchisimas gracias por jugar mi aventura! " + persona + "," + " " + "se vienen expansiones a la historia, con multiples finales y mayor interactividad!");
 }
 
 function ending2post(personaje, tamanoterreno, persona) {
 
-    alert("¡Llegaste al final! ahora, veamos un poco de estadisticas: " + "Tu personaje " + personaje + " " + "compro un terreno de tamaño " + tamanoterreno + " " + "sin embargo, decidio no comprar una casa prefabricada, sino que armar la suya propia.")
+    alert("¡Llegaste al final! ahora, veamos un poco de estadisticas: " + "Tu personaje " + personaje + " " + "compro un terreno de tamaño " + tamanoterreno + " " + "sin embargo, decidio no comprar una casa prefabricada, sino que armar la suya propia.");
     
-    alert("¡Muchisimas gracias por jugar mi aventura! " + persona + "," + " " + "se vienen expansiones a la historia, con multiples finales y mayor interactividad!")
-
+    alert("¡Muchisimas gracias por jugar mi aventura! " + persona + "," + " " + "se vienen expansiones a la historia, con multiples finales y mayor interactividad!");
 }
 
 // Milton te aviso que las funciones de ending se llevaron parte de mi sanidad mental
